46d60017ae2b9a1bd0a9c2d46d0113270bc9f893
NeelContractor <neelcontractor7@gmail.com>
2025-04-09 11:46:44 +0530

    fixed linting errors

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -1,26 +1,33 @@
 "use client";
 
-import React, { useState, useRef, Suspense } from "react";
+import React, { useState, useRef, Suspense, JSX } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { Points, PointMaterial } from "@react-three/drei";
+import * as THREE from "three";
+/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props: any) => {
-  const ref: any = useRef(null);
+const StarBackground = (props: JSX.IntrinsicElements['points']) => {
+  const ref = useRef<THREE.Points>(null);  
   const [sphere] = useState(() =>
     // random.inSphere(new Float32Array(5000), { radius: 1.2 })
     random.inSphere(new Float32Array(15000), { radius: 1.2 })
   );
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }    
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points 
+        /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+        // @ts-ignore
+        ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="$fff"
